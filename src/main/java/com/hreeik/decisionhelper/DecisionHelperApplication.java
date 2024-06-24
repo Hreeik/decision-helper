3e8455b63f862ca5332ddbc9d7ec28262ec605ff
Jan Čech <hreeik@gmail.com>
2024-06-24 22:12:37 +0200

    feat: init

---
+++ src/main/java/com/hreeik/decisionhelper/DecisionHelperApplication.java
@@ -0,0 +1,13 @@
+package com.hreeik.decisionhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hreeik/decisionhelper/DecisionHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hreeik.decisionhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
